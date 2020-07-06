ad4a0ceed57fc5b74ead15ce17f909926edc3442
Steven SH <stevensh@no8.io>
2020-07-07 00:02:45 +0800

    Implement Question 2

--- src/components/Question2.jsx
+++ src/components/Question2.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../assets/css/Question2.css';
+// import '../assets/css/Question2Answer.css';
 
 function Question2() {
   return (
-    <div>
+    <div className="Question2">
       <h1>Q2</h1>
+      <div className="Question2-content">
+        <div className="Question2-item">1</div>
+        <div className="Question2-item">2</div>
+        <div className="Question2-item">3</div>
+      </div>
     </div>
   );
 }
